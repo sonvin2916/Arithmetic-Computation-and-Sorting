9c3dc7a0892e7be6628b399a7fe99f613195ac48
sonvin2916 <sonalchougule07@gmail.com>
2020-06-10 22:35:17 +0530

    UC-4 : Added

--- arithmeticComp.sh
+++ arithmeticComp.sh
@@ -6,6 +6,9 @@ read -p "Enter the value of b : "  b
 read -p "Enter the value of c : "  c
 res1=$(( a + (b * c) ))
 res2=$(( (a * b) + c ))
+res3=$(( c + (a / b) ))
 echo "Result1 : " $res1
 echo "Result2 : " $res2
+echo $a $b $c | awk '{print $3 + ( $1 / $2 ) }'
+
 
