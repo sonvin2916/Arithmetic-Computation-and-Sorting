71e97807a87c9aaef1c3917d667bd6a7385d15b5
sonvin2916 <sonalchougule07@gmail.com>
2020-06-10 21:22:11 +0530

    UC-2 : Added

--- arithmeticComp.sh
+++ arithmeticComp.sh
@@ -4,4 +4,6 @@ echo "Arithmetic Computation and Sorting "
 read -p "Enter the value of a : "  a
 read -p "Enter the value of b : "  b
 read -p "Enter the value of c : "  c
+res1=$(( a + (b * c) ))
+echo "Result : " $res1
 
