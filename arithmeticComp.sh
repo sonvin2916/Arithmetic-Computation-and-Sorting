21c77cd9aaf60d605f4d3ed4dbaff2c72da04d2f
sonvin2916 <sonalchougule07@gmail.com>
2020-06-11 12:53:29 +0530

    UC-6 : Added

--- arithmeticComp.sh
+++ arithmeticComp.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 echo "Arithmetic Computation and Sorting "
+declare -A dic
 
 read -p "Enter the value of a : "  a
 read -p "Enter the value of b : "  b
@@ -7,8 +8,12 @@ read -p "Enter the value of c : "  c
 res1=$(( a + (b * c) ))
 res2=$(( (a * b) + c ))
 res3=$(( c + (a / b) ))
+res4=$(( (a % b) + c ))
 echo "Result1 : " $res1
 echo "Result2 : " $res2
-echo $a $b $c | awk '{print "Result3 : " $3 + ( $1 / $2 ) }'
+echo $a $b $c | awk '{print "Result3 : " $3 + ( $1 / $2 ) }' #res3=$(( c + (a / b) ))
+echo "Result4 : " $res4
+dic=([a1]=$res1 [a2]=$res2 [a3]=$res3 [a4]=$res4)
+echo ${dic[@]}
 
 
